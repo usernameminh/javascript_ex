79ac63289df65f6f319c8ad6aa971930689d2d07
minh chiu <minhch.vn@gmail.com>
2021-05-25 20:29:38 +0700

    [1.0.0] assignment_03

--- assignment_03/index.js
+++ assignment_03/index.js
@@ -1,160 +1,141 @@
-// create subject
-function createSubject(name, mark) {
-	return {
-		name: name,
-		mark: mark
+class Subjects {
+	constructor(name, grade) {
+		this.name = name.toLowerCase()
+		this.grade = grade
+	}
+
+	getGrade() {
+		return this.grade
+	}
+	getName() {
+		return this.name
 	}
 }
-class Student{
+
+class Student {
 	constructor(name, gender, subjects) {
 		this.name = name
 		this.gender = gender
 		this.subjects = subjects
 		this.mark = this.caculatorMark(subjects)
 	}
-	caculatorMark(subjects) {
-		var mark = subjects.reduce((total, currentValue) => {
-			return total+currentValue.mark
-		},0)
-		return mark/subjects.length
-	}
 
-	showInfo() {
-		return {
-			name: this.name,
-			gender: this.gender,
-			mark: this.mark,
-			subjects: this.subjects
-		}
+	caculatorMark(subjects) {
+		var mark = 0
+		subjects.forEach(subject => {
+			mark += subject.getGrade()
+		})
+		return mark/subjects.length
 	}
 
 	getMark() {
 		return this.mark
 	}
+
+	showInfo() {
+		return this.name +" - "+ this.mark
+	}
+
 	getGender() {
 		return this.gender
 	}
+
+	getName() {
+		return this.name
+	}
+
+	getMathOrPhysicsMark(subjects) {
+		subjects.forEach(subject => {
+			mark += subject.mark
+		})
+		return s
+	}
 }
 
-class StudentList {
-	createList() {
-		var studentList = [
-			new Student('thanh', 'nam' ,[createSubject('toan', 8), createSubject('Ly', 9)]),
-			new Student('cong',  'nam' ,[createSubject('toan', 8), createSubject('Ly', 8.9)]),
-			new Student('hanh',  'nu' ,[createSubject('toan', 9), createSubject('Ly', 8.8)]),
-			new Student('thuy',  'nu' ,[createSubject('toan', 9.2), createSubject('Ly', 7)]),
-			new Student('lien', 'nu' ,[createSubject('toan', 8), createSubject('Ly', 8.8)]),
-			new Student('tin',  'nam' ,[createSubject('toan', 6.2), createSubject('Ly', 10)]),
-			new Student('binh', 'nam' ,[createSubject('toan', 7), createSubject('Ly', 7)]),
-			new Student('vu',  'nam' ,[createSubject('toan', 9), createSubject('Ly', 8)]),
-		 	new Student('tung', 'nam' ,[createSubject('toan', 8), createSubject('Ly', 8.9)]),
-		 	new Student('ngao', 'nam' ,[createSubject('toan', 0), createSubject('Ly', 0)])
-		]
-		return studentList
+class StudentManage {
+	constructor(studentList) {
+		this.studentList = studentList
 	}
 
-	sortMarkMaxMin(studentList) {
-		for (var i = 0; i < studentList.length; i++) {
-			for (var j = i+1; j < studentList.length; j++) {
-				if(studentList[i].getMark() < studentList[j].getMark()) {
-					var temp = studentList[i]
-					studentList[i] = studentList[j]
-					studentList[j] = temp
-				}
+	add(...students) {
+		// this.studentList = [...this.studentList, ...students]
+		this.studentList = this.studentList.concat(students)
+		return this.studentList
+	}
+
+	sort(value = 'asc') {
+		this.studentList.sort((stdBefore, stdAfter) => {
+			if(value == 'desc') {
+				return stdAfter.getMark() - stdBefore.getMark()
 			}
-		}
+			return stdBefore.getMark() - stdAfter.getMark()
+		})
 	}
 
-	sortMarkMinMax(studentList) {
-		for (var i = 0; i < studentList.length; i++) {
-			for (var j = i+1; j < studentList.length; j++) {
-				if(studentList[i].getMark() > studentList[j].getMark()) {
-					var temp = studentList[i]
-					studentList[i] = studentList[j]
-					studentList[j] = temp
-				}
-			}
-		}
+
+	findByGenderAndMark(gender = 'nam', mark = 5) {
+		return this.studentList.filter((student) => {
+			return student.getGender() == gender && student.getMark() > mark
+		})
 	}
 
-	filterMale(studentList) {
-		let student = studentList.filter((student) => student.getMark() >= 5.0 && student.getGender() === 'nu')
-		return student
-	}
-
-	filterMarkEight(studentList) {
-		let student = studentList.filter((student) => {
-			var subjectMark = student.subjects.filter((subject, index, arr) => subject.mark > 8)
+	findBySubjectsAndGrade(subjects, grade) {
+		let student = this.studentList.filter((student, index) => {
+			var subjectMark = student.subjects.filter((subject, index) => {
+				return (subjects.includes(subject.getName())) && subject.getGrade() > grade
+			})
 			return student.subjects.includes(...subjectMark)
 		})
-		return student
+		 return student
 	}
 
-	searchMath(studentList){
-		var mathMax = 0
-		var mathMin = 10
-
-		// search student is math max
-		studentList.forEach((student, index, arr) => {
-			var mathSt = student.subjects.filter(subject => subject.name=='toan')[0].mark
-			if(mathMax < mathSt) {
-				mathMax = mathSt
-			}
+	findBySubjectGradeMax(nameSb) {
+		this.studentList.sort((student1, student2) => {
+			let gradeSt1 = student1.subjects.filter(subject => subject.getName() === nameSb.toLowerCase())[0].getGrade()
+			let gradeSt2 = student2.subjects.filter(subject => subject.getName() === nameSb.toLowerCase())[0].getGrade()
+			return gradeSt2 - gradeSt1
 		})
+		let maxGrade = this.studentList[0].subjects.filter(subject => subject.getName() === nameSb.toLowerCase())[0].getGrade()
 
-		// search student is math min
-		studentList.forEach((student, index, arr) => {
-			var mathSt= student.subjects.filter(subject => subject.name=='toan')[0].mark
-			if(mathMin > mathSt) {
-				mathMin = mathSt
-			}
+		let students = this.studentList.filter(student =>
+			student.subjects.filter( subject => subject.name.toLowerCase() == nameSb.toLowerCase())[0].getGrade() === maxGrade
+		)
+		return students
+	}
+
+	findBySubjectGradeMin(nameSb) {
+		this.studentList.sort((student1, student2) => {
+			let gradeSt1 = student1.subjects.filter(subject => subject.getName() === nameSb.toLowerCase())[0].getGrade()
+			let gradeSt2 = student2.subjects.filter(subject => subject.getName() === nameSb.toLowerCase())[0].getGrade()
+			return gradeSt1 - gradeSt2
 		})
+		let minGrade = this.studentList[0].subjects.filter(subject => subject.getName() === nameSb.toLowerCase())[0].getGrade()
 
-		var studentMathMax = studentList.filter(student => student.subjects.filter(subject => subject.name='toan')[0].mark == mathMax)
-		var studentMathMin = studentList.filter(student => student.subjects.filter(subject => subject.name='toan')[0].mark == mathMin)
-		return {
-			studentMathMin,
-			studentMathMax
-		}
+		let students = this.studentList.filter(student =>
+			student.subjects.filter( subject => subject.name.toLowerCase() == nameSb.toLowerCase())[0].getGrade() === minGrade
+		)
+		return students
 	}
 
-	removeStudentZero(studentList) {
-		studentList = studentList.filter((student) => student.getMark() !== 0)
-		return studentList
+	removeByMark(mark) {
+		this.studentList = this.studentList.filter(student => student.getMark() !== mark)
 	}
-	addStudent(studentList) {
-		let st1 = new Student('huy', 'nam' ,[createSubject('toan', 9), createSubject('Ly', 8)])
-		let st2 = new Student('tuan', 'nam' ,[createSubject('toan', 5), createSubject('Ly', 9)])
-		let st3 = new Student('ngan',  'nu' ,[createSubject('toan', 8), createSubject('Ly', 8.9)])
-		studentList.push(st1, st2, st3)
+	getStudentList() {
+		return this.studentList.map(student => {
+			return student
+		})
 	}
 }
 
-let stdList = new StudentList
-let listStudent = stdList.createList()
-
-
-var heading = document.getElementById('heading')
-var thead = document.getElementById('thead')
-var tbody = document.getElementById('tbody')
-var theadHtml = `
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Tên</th>
-      <th scope="col">Giới tính</th>
-      <th scope="col">Điểm trung bình</th>
-      <th scope="col">Môn học</th>
-    </tr>
-`
-let renderHTML = (studentList) => {
-	var studentHTML = studentList.map((student, index) => {
-			var subject= student.subjects.map(subject => `<span class='ml-1 mr-1'>${subject.name}: ${subject.mark},</span>`)
+const renderHTML = (studentList) => {
+	let studentHTML = studentList.map((student, index) => {
+			let subject= student.subjects.map(subject => `<span class='ml-1 mr-1'>${subject.getName()}: ${subject.getGrade()},</span>`)
 			return `
 				 <tr>
 				      <th scope="row">${index+1}</th>
-				      <td>${student.name}</td>
-				      <td>${student.gender}</td>
-				      <td>${student.mark}</td>
+				      <td>${student.getName()}</td>
+				      <td>${student.getGender()}</td>
+				      <td>${student.getMark()}</td>
 				      <td>${subject.join('')}</td>
 				 </tr>
 			`
@@ -163,67 +144,100 @@ let renderHTML = (studentList) => {
 	return studentHTML.join('')
 }
 
-
-
-var showList = document.getElementById('show-list')
-var sortMaxMin = document.getElementById('sort-max-min')
-var sortMinMax = document.getElementById('sort-min-max')
-var listMaleMark = document.getElementById('list-male-mark')
-var subjectMathPhysical = document.getElementById('subject-math-physical')
-var subjectMath = document.getElementById('subject-math')
-var removeStudent = document.getElementById('remove-student')
-var addStudent = document.getElementById('add-student')
-
-
-showList.onclick = () => {
-	heading.textContent = 'Danh sách sinh viên của một lớp có 10 sinh viên'
+runHTML = ( studentList ,headingText, headingSelector, theadSelector, tbodySelector) => {
+	let theadHtml = `
+	  <tr>
+	    <th scope="col">#</th>
+	    <th scope="col">Tên</th>
+	    <th scope="col">Giới tính</th>
+	    <th scope="col">Điểm trung bình</th>
+	    <th scope="col">Môn học</th>
+	  </tr>
+	`
+	heading.textContent = headingText
 	thead.innerHTML = theadHtml
-	tbody.innerHTML = renderHTML(stdList.createList())
+	tbody.innerHTML = renderHTML(studentList)
 }
 
-sortMaxMin.onclick = () => {
-	stdList.sortMarkMaxMin(listStudent)
-	heading.textContent = 'Sắp xếp danh sách sinh viên theo thứ tự điểm tích luỹ từ lớn đến bé'
-	thead.innerHTML = theadHtml
-	tbody.innerHTML = renderHTML(listStudent)
+var showBtn 				= document.getElementById('show-list')
+var sortMaxMinBtn 	= document.getElementById('sort-max-min')
+var sortMinMaxBtn 	= document.getElementById('sort-min-max')
+var listMaleAndMarkBtn = document.getElementById('list-male-mark')
+var showSubjectsBtn =	document.getElementById('subject-math-physical')
+var showMathMaxBtn 		= document.getElementById('subject-math-max')
+var showMathMinBtn 		= document.getElementById('subject-math-min')
+var removeBtn  			= document.getElementById('remove-student')
+var addBtn 					= document.getElementById('add-student')
+
+
+let heading = document.getElementById('heading')
+let thead 	= document.getElementById('thead')
+let tbody 	= document.getElementById('tbody')
+
+
+let studentList = [
+	new Student('Tung', 'nam', [ new Subjects('Ly', 8), new Subjects('Toan', 8) ]),
+	new Student('Nam', 'nam', [ new Subjects('Ly', 9), new Subjects('Toan', 9) ]),
+	new Student('Hoai', 'nu', [ new Subjects('Ly', 8), new Subjects('Toan', 7) ]),
+	new Student('Thuan', 'nam', [ new Subjects('Ly', 8), new Subjects('Toan', 9) ]),
+	new Student('Linh', 'nu', [ new Subjects('Ly', 8), new Subjects('Toan', 7) ]),
+	new Student('Hanh', 'nu', [ new Subjects('Ly', 8), new Subjects('Toan', 10) ]),
+	new Student('Tuan', 'nam', [ new Subjects('Ly', 8), new Subjects('Toan', 6) ]),
+]
+let students = new StudentManage(studentList)
+
+
+// 1/ Tạo danh sách sinh viên của một lớp có 10 sinh viên
+showBtn.onclick = () => {
+	runHTML(students.getStudentList(), 'Danh sách sinh viên', heading, thead, tbody)
 }
 
-sortMinMax.onclick = () => {
-	stdList.sortMarkMinMax(listStudent)
-	heading.textContent = 'Sắp xếp danh sách sinh viên theo thứ tự điểm tích luỹ từ bé đến lớn'
-	thead.innerHTML = theadHtml
-	tbody.innerHTML = renderHTML(listStudent)
+// 2/ Sắp xếp danh sách sinh viên theo thứ tự điểm tích luỹ từ lớn đến bé
+sortMaxMinBtn.onclick = () => {
+	students.sort('desc')
+	runHTML(students.getStudentList(), 'Danh sách sinh viên', heading, thead, tbody)
 }
 
-listMaleMark.onclick = () => {
-	heading.textContent = 'Lọc ra danh sách sinh viên là nữ và có điểm tích luỹ lớn hơn 5.0'
-	thead.innerHTML = theadHtml
-	tbody.innerHTML = renderHTML(stdList.filterMale(listStudent))
+// 3/ Sắp xếp danh sách sinh viên theo thứ tự điểm tích luỹ từ bé đến lớn
+sortMinMaxBtn.onclick = () => {
+	students.sort()
+	runHTML(students.getStudentList(), 'Danh sách sinh viên', heading, thead, tbody)
 }
 
-subjectMathPhysical.onclick = () => {
-	heading.textContent = 'Lọc ra danh sách sinh viên có điểm toán hoặc lý lớn hơn 8.0'
-	thead.innerHTML = theadHtml
-	tbody.innerHTML = renderHTML(stdList.filterMarkEight(listStudent))
+// 4/ Lọc ra danh sách sinh viên là nữ và có điểm tích luỹ lớn hơn 5.0
+listMaleAndMarkBtn.onclick = () => {
+	let studentMale = students.findByGenderAndMark('nu', 5 )
+	runHTML(studentMale, 'Danh sách sinh viên', heading, thead, tbody)
 }
 
-subjectMath.onclick = () => {
-	var studentMath = [...stdList.searchMath(listStudent).studentMathMax, ...stdList.searchMath(listStudent).studentMathMin]
-	heading.textContent = 'Sinh viên có điểm môn Toán cao nhất và thấp nhất'
-	thead.innerHTML = theadHtml
-	tbody.innerHTML = renderHTML(studentMath)
+// 5/ Lọc ra danh sách sinh viên có điểm toán hoặc lý lớn hơn 8.0
+showSubjectsBtn.onclick = () => {
+	let studentMathAndPhysical =	students.findBySubjectsAndGrade(['toan', 'ly'], 8.1)
+	runHTML(studentMathAndPhysical, 'Danh sách sinh viên', heading, thead, tbody)
 }
 
-removeStudent.onclick = () => {
-	listStudent = stdList.removeStudentZero(listStudent)
-	heading.textContent = 'Xoá khỏi danh sách sinh viên có điểm tích luỹ là 0'
-	thead.innerHTML = theadHtml
-	tbody.innerHTML = renderHTML(listStudent)
+// 6/ Tìm sinh viên có điểm môn Toán cao nhất
+showMathMaxBtn.onclick = () => {
+	let studentMathMax = students.findBySubjectGradeMax('toan')
+	runHTML(studentMathMax, 'Danh sách sinh viên', heading, thead, tbody)
 }
 
-addStudent.onclick = () => {
-	stdList.addStudent(listStudent)
-	heading.textContent = 'Thêm 03 sinh viên mới vào danh sách.'
-	thead.innerHTML = theadHtml
-	tbody.innerHTML = renderHTML(listStudent)
+// 6/ Tìm sinh viên có điểm môn Toán thấp nhất
+showMathMinBtn.onclick = () => {
+	let studentMathMin = students.findBySubjectGradeMin('toan')
+	runHTML(studentMathMin, 'Danh sách sinh viên', heading, thead, tbody)
 }
+
+// 7/ Xoá khỏi danh sách sinh viên có điểm tích luỹ là 0
+removeBtn.onclick = () => {
+	students.removeByMark(0)
+	runHTML(students.getStudentList(), 'Danh sách sinh viên', heading, thead, tbody)
+}
+
+// 8/ Thêm 03 sinh viên mới vào danh sách.
+addBtn.onclick = () => {
+	students.add(new Student('HI', 'nu', [ new Subjects('Ly', 0), new Subjects('Toan', 0) ]))
+	runHTML(students.getStudentList(), 'Danh sách sinh viên', heading, thead, tbody)
+}
+
+
